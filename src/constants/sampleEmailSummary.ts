e3d0ef00dcd9678f568dc3338502bcd250c77daf
Shishir <shishir3324kumar@gmail.com>
2024-06-21 22:23:12 +0530

    feature: create emailPreviewType and generated sample email summaries as constant

---
+++ src/constants/sampleEmailSummary.ts
@@ -0,0 +1,32 @@
+import { EmailPreviewProps } from "../types/ComponentProps"
+
+import profile1 from "../assets/profile1.jpeg"
+import profile2 from "../assets/profile2.jpeg"
+import profile3 from "../assets/profile3.jpeg"
+
+const validColors = ["primary.main", "success.main", "info.main"]
+
+function generateRandomValues(arr : string[]) {
+    let rand = Math.floor(Math.random() * arr.length)
+
+    return arr[rand];
+}
+
+const generateColor = () => generateRandomValues(validColors)
+
+export const emailSummaries : EmailPreviewProps[] = [
+    {
+        senderName: "John Wright",
+        senderEmail: "john12@gmail.com",
+        subject: "Welcome to our beautiful community",
+        dateReceived: new Date(2024, 6, 20),
+        profilePic: profile1,
+    },
+    {
+        senderName: "MLH Fellowship",
+        senderEmail: "fellowship@mlh.io",
+        subject: "MLH Fellowship welcomes you!",
+        dateReceived: new Date(2024, 6, 19),
+        themeColor: generateColor(),
+    }
+]
--- src/types/ComponentProps.ts
+++ src/types/ComponentProps.ts
@@ -1,22 +1,30 @@
 import { SxProps, Theme } from "@mui/material/styles";
 
 export interface BoxProps {
-    children: React.ReactNode;
-    style?: SxProps;
+  children: React.ReactNode;
+  style?: SxProps;
 }
 
 export interface TagProps {
-    name: String,
-    color: String,
+  name: String;
+  color: String;
 }
 
 export interface DrawerProps {
-    isOpen: boolean,
-    handleClose: () => void
+  isOpen: boolean;
+  handleClose: () => void;
 }
 
 export interface UserNavProps {
-    isOpen: boolean,
-    handleOpen: () => void
+  isOpen: boolean;
+  handleOpen: () => void;
 }
 
+export interface EmailPreviewProps {
+  senderEmail: string;
+  senderName: string;
+  subject: string;
+  dateReceived: Date;
+  themeColor?: string;
+  profilePic?: string;
+}
